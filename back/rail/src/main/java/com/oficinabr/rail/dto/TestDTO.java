2042f5da15389adbfb0a91add890f9f7676004af
Aroldo Costa <aroldocosta@yahoo.com.br>
2023-12-04 22:21:08 -0300

    Correção no parametro identificação do injetor de code para model

--- back/rail/src/main/java/com/oficinabr/rail/dto/TestDTO.java
+++ back/rail/src/main/java/com/oficinabr/rail/dto/TestDTO.java
@@ -4,7 +4,7 @@ import com.oficinabr.rail.entity.Test;
 
 public record TestDTO(String id, Long sequence, String date, Float resistance, Float inductance, Float isolation,
 		Float halfLoad, Float fullLoad, Float idling, Float preInjection, Float halfLoadReturn, Float fullLoadReturn,
-		Float idlingReturn, Float preInjectionReturn, String planId, String injectorId, String injectorCode,
+		Float idlingReturn, Float preInjectionReturn, String planId, String injectorId, String injectorModel,
 		String vehicleId, String vehiclePlate, String imaCode, String serviceOrder
 
 ) {
@@ -12,7 +12,7 @@ public record TestDTO(String id, Long sequence, String date, Float resistance, F
 		this(t.getId(), t.getSequence(), t.getDate(), t.getResistance(), t.getInductance(), t.getIsolation(),
 				t.getHalfLoad(), t.getFullLoad(), t.getIdling(), t.getPreInjection(), t.getHalfLoadReturn(),
 				t.getFullLoadReturn(), t.getIdlingReturn(), t.getPreInjectionReturn(), t.getPlan().getId(),
-				t.getInjector().getId(), t.getInjector().getCode(), t.getVehicle().getId(), t.getVehicle().getPlate(),
+				t.getInjector().getId(), t.getInjector().getModel(), t.getVehicle().getId(), t.getVehicle().getPlate(),
 				t.getImaCode(), t.getServiceOrder());
 	}
 
