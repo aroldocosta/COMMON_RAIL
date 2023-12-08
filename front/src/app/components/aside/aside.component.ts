19fe9adad0d210820857474fcf3a015058015867
Aroldo Costa <aroldocosta@yahoo.com.br>
2023-12-08 08:11:54 -0300

    Ajustes na interface e gaficos de resultados na coluna lateral

--- front/src/app/components/aside/aside.component.ts
+++ front/src/app/components/aside/aside.component.ts
@@ -18,6 +18,8 @@ export class AsideComponent implements OnInit {
   gaugeH = 0;
   gaugeY = 135;
 
+  
+
   // @ViewChild('gauge', { static: false, read: HTMLCanvasElement }) gauge!: HTMLCanvasElement;
   // @ViewChild('gaugeCanvas') gaugeCanvas!: ElementRef;
   @ViewChild('debCanvas', {static: false}) debCanvas!: ElementRef;
@@ -36,25 +38,24 @@ export class AsideComponent implements OnInit {
     this.retContxt = this.retCanvas.nativeElement.getContext('2d');
   }
 
-  drawGauge(contxt: CanvasRenderingContext2D, maxValue: string, minValue: string, measuredValue: string) {
+  drawGauge(contxt: CanvasRenderingContext2D, maxValue: string, minValue: string, value: string) {
     let colHeight = 130;
     let red = 0;
     let grn = 255;
     let blu = 0;
 
-    let val = Number(measuredValue);
+    let val = Number(value);
     let min = Number(minValue);
     let max = Number(maxValue);
 
     let n_val = 100*val/max;
-    
     let n_min = 100*min/max;
-
     let n_max = 100;
-
     let n_ref = 100*((max+min)/2)/max;
 
-    let col = (colHeight * n_val / 100);
+    let col  = (colHeight * n_val / 100);
+    let med1 = (n_ref + n_min)/2;
+    let med2 = (n_ref + n_max)/2;
 
     if(val < min) {
       red = 255;
@@ -64,15 +65,13 @@ export class AsideComponent implements OnInit {
       red = 255;
       grn = 0;
       blu = 0;
-    } else {
-      
-      
-      if(n_val < (n_ref + min)/2) {//greenyellow
+    } else { 
+      if(n_val < med1) {//greenyellow
         red = 173;
         grn = 255;
         blu = 47;
         //rrgb(173,255,47)
-      } else if(n_val < (n_ref + max)/2) { //green - rgb(0,255,0)
+      } else if(n_val < med2) { //green - rgb(0,255,0)
         red = 0;
         grn = 255;
         blu = 0;
